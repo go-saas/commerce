c0ed2be81d43006345284686def4f0e404637d9b
Goxiaoy <xy1996mail@gmail.com>
2023-02-11 15:30:27 +0800

    remove web

--- web/src/utils/qiankunJump.ts
+++
@@ -1,6 +0,0 @@
-import { history as umiHistory } from 'umi';
-
-export const qiankunJump = (url: string, name = '', params = null) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  window.__POWERED_BY_QIANKUN__ ? history.pushState(params, name, url) : umiHistory.push(url);
-};
